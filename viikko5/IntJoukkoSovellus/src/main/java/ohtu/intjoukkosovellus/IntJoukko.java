3b306de74fb644cec7959952b2b8082f51019362
PyryV <pyry.vekkeli@helsinki.fi>
2018-12-02 16:01:27 +0200

    Viikon 5 tehtävät 1 ja 2

--- viikko5/IntJoukkoSovellus/src/main/java/ohtu/intjoukkosovellus/IntJoukko.java
+++ viikko5/IntJoukkoSovellus/src/main/java/ohtu/intjoukkosovellus/IntJoukko.java
@@ -193,7 +193,7 @@ public class IntJoukko {
         return erotus;
     }
 
-
+    
     
         
 }
--- viikko5/Tennis/src/main/java/ohtu/TennisGame.java
+++ viikko5/Tennis/src/main/java/ohtu/TennisGame.java
@@ -6,10 +6,14 @@ public class TennisGame {
     private int m_score2 = 0;
     private String player1Name;
     private String player2Name;
+    private String[] tieScores;
+    private String[] nonTieScores;
 
     public TennisGame(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
+        tieScores = createTieScores();
+        nonTieScores = createNonTieScores();
     }
 
     public void wonPoint(String playerName) {
@@ -19,62 +23,84 @@ public class TennisGame {
             m_score2 += 1;
     }
 
+    private String[] createTieScores() {
+        String[] tieScores = {
+            "Love-All",
+            "Fifteen-All",
+            "Thirty-All",
+            "Forty-All",
+            "Deuce"
+        };
+        return tieScores;
+    }
+
+    private String[] createNonTieScores() {
+        String[] nonTieScores = {
+            "Love",
+            "Fifteen",
+            "Thirty",
+            "Forty"
+        };
+        return nonTieScores;
+    }
+
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
+        if (m_score1==m_score2) {
+            return getTieScore();
+        } else if (m_score1>=4 || m_score2>=4) {
             int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            if (minusResult==1 || minusResult ==-1) return getAdvantage(minusResult);
+            else return getWinner(minusResult);
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+            return getNonTieScore();
+    }
+
+    private String getAdvantage(int minusResult) {
+        if(minusResult == 1) return "Advantage for player1";
+        return "Advantage for player2";
+    }
+
+    private String getWinner(int minusResult) {
+        if(minusResult > 1) return "Win for player1";
+        return "Win for player2";
+    }
+
+    private String getNonTieScore() {
+        int tempScore=0;
+        String score = "";
+        for (int i=1; i<3; i++) {
+            if (i==1) tempScore = m_score1;
+            else { score+="-"; tempScore = m_score2;}
+            score+=nonTieScores[tempScore];
         }
         return score;
+    }
+
+    private String getTieScore() {
+        return tieScores[m_score1];
+    }
+
+
+
+    public static void main(String[] args) {
+        TennisGame game = new TennisGame("player1", "player2");
+    
+        System.out.println(game.getScore());
+    
+        game.wonPoint("player1");
+        System.out.println(game.getScore());
+    
+        game.wonPoint("player1");
+        System.out.println(game.getScore());
+    
+        game.wonPoint("player2");
+        System.out.println(game.getScore());
+    
+        game.wonPoint("player1");
+        System.out.println(game.getScore());
+    
+        game.wonPoint("player1");
+        System.out.println(game.getScore());
     }
 }
