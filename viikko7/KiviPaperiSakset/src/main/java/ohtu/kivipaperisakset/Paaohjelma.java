1377b3e0a713ab946995136bd9d3eda106cce26d
PyryV <pyry.vekkeli@helsinki.fi>
2018-12-15 18:27:19 +0200

    1. tiivistelmä

---
+++ viikko7/KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/Paaohjelma.java
@@ -0,0 +1,36 @@
+package ohtu.kivipaperisakset;
+
+import java.util.Scanner;
+
+public class Paaohjelma {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        while (true) {
+            System.out.println("\nValitse pelataanko"
+                    + "\n (a) ihmistä vastaan "
+                    + "\n (b) tekoälyä vastaan"
+                    + "\n (c) parannettua tekoälyä vastaan"
+                    + "\nmuilla valinnoilla lopetataan");
+
+            String vastaus = scanner.nextLine();
+            System.out.println("peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s");
+            if (vastaus.endsWith("a")) {
+                KPSPelaajaVsPelaaja kaksinpeli = new KPSPelaajaVsPelaaja();
+                kaksinpeli.pelaa();
+            } else if (vastaus.endsWith("b")) {
+                KPSTekoaly yksinpeli = new KPSTekoaly();
+                yksinpeli.pelaa();
+            } else if (vastaus.endsWith("c")) {
+                KPSParempiTekoaly pahaYksinpeli = new KPSParempiTekoaly();
+                pahaYksinpeli.pelaa();
+            } else {
+                break;
+            }
+
+        }
+
+    }
+}
